7ccc70a42e898c1aef8f2c9e06414b7d010329dc
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2020-09-25 09:21:47 -0300

    add controllers

---
+++ app/Http/Controllers/EntraceController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Entrace;
+use Illuminate\Http\Request;
+
+class EntraceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Entrace  $entrace
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Entrace $entrace)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Entrace  $entrace
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Entrace $entrace)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Entrace  $entrace
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Entrace $entrace)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UsersHasWalletsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UsersHasWallets;
+use Illuminate\Http\Request;
+
+class UsersHasWalletsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UsersHasWallets  $usersHasWallets
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UsersHasWallets $usersHasWallets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UsersHasWallets  $usersHasWallets
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UsersHasWallets $usersHasWallets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UsersHasWallets  $usersHasWallets
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UsersHasWallets $usersHasWallets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UsersHasWallets  $usersHasWallets
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UsersHasWallets $usersHasWallets)
+    {
+        //
+    }
+}
